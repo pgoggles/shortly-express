fafb4d7dc4de6ac259352c468e8aca483e13859c
pgoggles <praneeth.gogineni@gmail.com>
2021-04-02 16:50:57 -0700

    Implement Username and Password Storage and Retrieval

--- server/app.js
+++ server/app.js
@@ -17,62 +17,106 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/',
+  (req, res) => {
+    res.render('login');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/login',
+  (req, res) => {
+    res.render('login');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
 
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
+app.get('/signup',
+  (req, res) => {
+    res.render('signup');
+  });
 
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.post('/signup',
+  (req, res) => {
+
+    models.Users.create(req.body)
+      .then((data) => {
+        res.redirect('/login');
+      })
+      .catch((err) => {
+        res.status(400).render('signup');
       });
+    // res.render('signup');
+  });
+
+app.post('/login', (req, res) => {
+  models.UsersDB.get({username: req.body.username})
+    .then((data) => {
+      if (data) {
+        return models.Users.compare(req.body.password, data.password, data.salt);
+      } else {
+        return false;
+      }
     })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
+    .then((isUser) => {
+      if (isUser) {
+        res.render('index');
+      } else {
+        res.render('login');
+      }
+
     });
 });
+
+
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
+
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
+      });
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+    // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
--- server/models/index.js
+++ server/models/index.js
@@ -2,3 +2,6 @@ module.exports.Links = require('./link');
 module.exports.Clicks = require('./click');
 module.exports.Sessions = require('./session');
 module.exports.Users = require('./user');
+module.exports.UsersDB = require('./users');
+
+
